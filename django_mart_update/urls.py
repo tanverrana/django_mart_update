e500f483794592842f12ee3c6198a6feb7e7458d
pro <pro@prodeMacBook-Pro-2.local>
2023-09-28 00:34:31 +0600

    define app urls

--- django_mart_update/urls.py
+++ django_mart_update/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('accounts.urls')),
+    path('store/', include('store.urls')),
+    path('orders/', include('orders.urls')),
+    path('category/', include('category.urls')),
+    path('cart/', include('cart.urls')),
 ]
