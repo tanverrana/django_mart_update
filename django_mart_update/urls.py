cc586974915cfbc19fee7e39c7865b70401c4750
pro <pro@prodeMacBook-Pro-2.local>
2023-09-30 01:21:30 +0600

    home page define

--- django_mart_update/urls.py
+++ django_mart_update/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('account/', include('accounts.urls')),
     path('store/', include('store.urls')),
     path('order/', include('orders.urls')),
