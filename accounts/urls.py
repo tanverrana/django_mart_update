d9ffea84240477b6329a384b3e6aec219b7a1746
pro <pro@prodeMacBook-Pro-2.local>
2023-09-29 23:43:59 +0600

    base.html and show dashboard file

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('register/', views.register),
+    path('profile/', views.profile),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def register(request):
     return render(request, 'accounts/register.html')
+
+
+def profile(request):
+    return render(request, 'accounts/dashboard.html')
