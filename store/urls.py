97c91899535e1069da9a5e2881ce532ba1a16409
pro <pro@prodeMacBook-Pro-2.local>
2023-10-02 12:19:49 +0600

    show categories

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.store, name='store'),
+    path('category/<slug:category_slug>/',
+         views.store, name='products_by_category'),
     path('product_detail/', views.product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from category.models import Category
 # Create your views here.
 
 
-def store(request):
-    products = Product.objects.filter(is_available=True)
-    return render(request, 'store/store.html', {'products': products})
+def store(request, category_slug=None):
+
+    products = None
+    category = None
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(is_available=True, category=category)
+    else:
+        products = Product.objects.filter(is_available=True)
+    categories = Category.objects.all()
+    context = {'products': products, 'categories': categories}
+    return render(request, 'store/store.html', context)
 
 
 def product_detail(request):
