0ac023039222a98262992e508d2b0c22ae98dbcf
pro <pro@prodeMacBook-Pro-2.local>
2023-10-03 01:31:09 +0600

    add to cart working

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from store.models import Product
 # Create your views here.
+from .models import Cart, CartItem
 
 
 def cart(request):
@@ -9,4 +10,15 @@ def cart(request):
 
 def add_to_cart(request, product_id):
     product = Product.objects.get(id=product_id)
+    cart_id = request.session.session_key
+    cart = Cart.objects.create(
+        cart_id=cart_id
+    )
+    cart.save()
+    cart_item = CartItem.objects.create(
+        product=product,
+        quantity=1,
+        cart=cart
+    )
+    cart_item.save()
     return redirect('cart')
