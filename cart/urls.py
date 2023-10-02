9c56a3d81abfadb5ca1569cb656bc181d9232eea
pro <pro@prodeMacBook-Pro-2.local>
2023-10-03 01:04:36 +0600

    show cart product

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.cart, name='cart'),
+    path('<int:product_id>/', views.add_to_cart, name='add_cart'),
 
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from store.models import Product
 # Create your views here.
 
 
 def cart(request):
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return redirect('cart')
