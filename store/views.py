388d0b98a882bba176ac6adff8709c848fcf79e8
pro <pro@prodeMacBook-Pro-2.local>
2023-10-02 11:29:32 +0600

    show products in UI

--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.filter(is_available=True)
+    return render(request, 'store/store.html', {'products': products})
 
 
 def product_detail(request):
