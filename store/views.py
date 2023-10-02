da4f2f7f85808605541e76f917c4b6c05a521ab4
pro <pro@prodeMacBook-Pro-2.local>
2023-10-02 23:28:36 +0600

    add page paginator

--- store/views.py
+++ store/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def store(request, category_slug=None):
-
-    products = None
-    category = None
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(is_available=True, category=category)
+        page = request.GET.get('page')
+        paginator = Paginator(products, 2)
+        paged_product = paginator.get_page(page)
     else:
         products = Product.objects.filter(is_available=True)
+        paginator = Paginator(products, 2)
+        page = request.GET.get('page')
+        paged_product = paginator.get_page(page)
     categories = Category.objects.all()
-    context = {'products': products, 'categories': categories}
+    context = {'products': paged_product, 'categories': categories}
     return render(request, 'store/store.html', context)
 
 
