ccfaac0ef358ef060e824a73dcfa1b06cafce847
pro <pro@prodeMacBook-Pro-2.local>
2023-10-03 10:23:45 +0600

    add to cart

--- cart/models.py
+++ cart/models.py
@@ -21,4 +21,4 @@ class CartItem(models.Model):
         return self.product.price*self.quantity
 
     def __str__(self):
-        return self.product
+        return str(self.product)
--- cart/views.py
+++ cart/views.py
@@ -10,15 +10,27 @@ def cart(request):
 
 def add_to_cart(request, product_id):
     product = Product.objects.get(id=product_id)
-    cart_id = request.session.session_key
-    cart = Cart.objects.create(
-        cart_id=cart_id
-    )
-    cart.save()
-    cart_item = CartItem.objects.create(
-        product=product,
-        quantity=1,
-        cart=cart
-    )
-    cart_item.save()
+    print('add to cart', product)
+    session_id = request.session.session_key
+    cart_id = Cart.objects.filter(cart_id=session_id).exists()
+    if cart_id:
+        cart_item = CartItem.objects.filter(product=product).exists()
+        if cart_item:
+            item = CartItem.objects.get(product=product)
+            item.quantity += 1
+            item.save()
+        else:
+            cartid = Cart.objects.get(cart_id=session_id)
+            item = CartItem.objects.create(
+                cart=cartid,
+                product=product,
+                quantity=1
+            )
+            item.save()
+    else:
+        cart = Cart.objects.create(
+            cart_id=session_id
+        )
+        cart.save()
+
     return redirect('cart')
